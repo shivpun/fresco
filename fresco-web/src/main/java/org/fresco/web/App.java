197c8b9376dbfdc5e7c5ebe966e5d3a266c9d3d0
Punit Tiwari <punittiwari58@gmail.com>
2019-02-19 21:43:50 +0530

    Enable Async via spring annotation

--- fresco-web/src/main/java/org/fresco/web/App.java
+++ fresco-web/src/main/java/org/fresco/web/App.java
@@ -2,7 +2,9 @@ package org.fresco.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
